048efd626aa578f00268b78b060fe0cb65b3950c
arina86 <110954915+arina86@users.noreply.github.com>
2023-08-31 19:07:45 +0800

    Add files via upload
    Add layout and pages files that I have modified

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import NavBar from '@/components/Navbar'
+import Hero from '@/components/Hero'
+import Features from '@/components/Features'
+
+export default function Home() {
+  return (
+    <>
+      <NavBar />
+      <Hero />
+      <Features />
+    </>
+
+  )
+}
